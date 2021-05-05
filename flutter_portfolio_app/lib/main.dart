29dd2a405cfd9555ccc643f38eb27f75d4e9efc5
DebyGrey <demifashoro@gmail.com>
2021-05-05 10:47:25 +0100

    recent may5

--- flutter_portfolio_app/lib/main.dart
+++ flutter_portfolio_app/lib/main.dart
@@ -1,113 +1,288 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(PortfolioApp());
 }
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class PortfolioApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.white70,
+          title: Text(
+            'Portfolio',
+            textDirection: TextDirection.ltr,
+          ),
+        ),
+        body: ListView(
+          scrollDirection: Axis.vertical,
+          children: [
+            Center(
+              child: SafeArea(
+                child: Column(
+                  children: [
+                    Container(
+                      alignment: Alignment.topLeft,
+                      padding: EdgeInsets.all(10),
+                      margin: EdgeInsets.all(10),
+                      child: Column(
+                        children: [
+                          //Icon and "Hello" text row
+                          Row(
+                            children: [
+                              Container(
+                                child: Icon(
+                                  Icons.favorite,
+                                  color: Colors.pink,
+                                  size: 24.0,
+                                ),
+                              ),
+                              Container(
+                                child: Text(
+                                  "Hello",
+                                  style: TextStyle(
+                                    fontSize: 20,
+                                  ),
+                                ),
+                                padding: EdgeInsets.all(20),
+                              ),
+                            ],
+                          ),
+                          //Intro row
+                          Row(
+                            children: [
+                              Expanded(
+                                child: Container(
+                                  child: Text(
+                                    "I\'m Deborah. I create value and solve problems as a Software Developer.",
+                                    style: TextStyle(
+                                      fontSize: 28,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                    //photo row
+                    Row(
+                      children: [
+                        Expanded(
+                          child: Container(
+                            child: Image(
+                              image: NetworkImage(
+                                  'https://flutter.github.io/assets-for-api-docs/assets/widgets/owl.jpg'),
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+                    Container(
+                      alignment: Alignment.topLeft,
+                      margin: const EdgeInsets.all(15.0),
+                      padding: const EdgeInsets.all(10.0),
+                      child: Text(
+                        'What I do',
+                        textAlign: TextAlign.left,
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18,
+                        ),
+                      ),
+                    ),
+                    //what I do row
+                    Row(
+                      children: [
+                        Container(
+                          margin: const EdgeInsets.all(15.0),
+                          padding: const EdgeInsets.all(10.0),
+                          decoration: BoxDecoration(
+                              border: Border.all(color: Colors.blueAccent)),
+                          height: 100,
+                          width: 140,
+                          child: Column(
+                            children: [
+                              Row(
+                                children: [
+                                  //First box
+                                  Container(
+                                    child: Icon(
+                                      Icons.web_outlined,
+                                      color: Colors.blueAccent,
+                                      size: 40.0,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              Row(
+                                children: [
+                                  Container(
+                                    child: Text(
+                                      "Web Development",
+                                      style: TextStyle(
+                                        fontSize: 14,
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ),
+                        //Second box
+                        Container(
+                          margin: const EdgeInsets.all(15.0),
+                          padding: const EdgeInsets.all(10.0),
+                          decoration: BoxDecoration(
+                              border: Border.all(color: Colors.blueAccent)),
+                          height: 100,
+                          width: 140,
+                          child: Column(
+                            children: [
+                              Row(
+                                children: [
+                                  Container(
+                                    child: Icon(
+                                      Icons.web_outlined,
+                                      color: Colors.blueAccent,
+                                      size: 40.0,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              Row(
+                                children: [
+                                  Container(
+                                    child: Text(
+                                      "Web Development",
+                                      style: TextStyle(
+                                        fontSize: 14,
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ),
+                        //Third box
+                        // Container(
+                        //   margin: const EdgeInsets.all(15.0),
+                        //   padding: const EdgeInsets.all(10.0),
+                        //   decoration: BoxDecoration(
+                        //       border: Border.all(color: Colors.blueAccent)),
+                        //   height: 100,
+                        //   width: 140,
+                        //   child: Column(
+                        //     children: [
+                        //       Row(
+                        //         children: [
+                        //           Container(
+                        //             child: Icon(
+                        //               Icons.web_outlined,
+                        //               color: Colors.blueAccent,
+                        //               size: 40.0,
+                        //             ),
+                        //           ),
+                        //         ],
+                        //       ),
+                        //       Row(
+                        //         children: [
+                        //           Container(
+                        //             child: Text(
+                        //               "Web Development",
+                        //               style: TextStyle(
+                        //                 fontSize: 14,
+                        //               ),
+                        //             ),
+                        //           ),
+                        //         ],
+                        //       ),
+                        //     ],
+                        //   ),
+                        // ),
+                      ],
+                    ),
+                    Container(
+                        alignment: Alignment.topLeft,
+                        margin: const EdgeInsets.all(15.0),
+                        padding: const EdgeInsets.all(10.0),
+                        child: Text(
+                          'Work experience',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 18,
+                          ),
+                          textAlign: TextAlign.left,
+                        )),
+                    //Experience row
+                    Row(
+                      children: [
+                        Expanded(
+                          child: Container(
+                              // child: Image(
+                              //   image: NetworkImage(
+                              //       'https://flutter.github.io/assets-for-api-docs/assets/widgets/owl.jpg'),
+                              // ),
+                              ),
+                        )
+                      ],
+                    ),
+                    //Personal journey row
+                    Row(
+                      children: [
+                        Expanded(
+                          child: Container(
+                              //   child: Image(
+                              //     image: NetworkImage(
+                              //         'https://flutter.github.io/assets-for-api-docs/assets/widgets/owl.jpg'),
+                              //   ),
+                              ),
+                        )
+                      ],
+                    ),
+                    //About me row
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        Container(
+                          height: 50,
+                          width: 100,
+                        ),
+                        Card(),
+                        Card(),
+                      ],
+                    ),
+                    //Contact me
+                    Row(
+                      children: [
+                        Expanded(
+                          child: Container(
+                              // child: Image(
+                              //   image: NetworkImage(
+                              //       'https://flutter.github.io/assets-for-api-docs/assets/widgets/owl.jpg'),
+                              // ),
+                              ),
+                        )
+                      ],
+                    ),
+                  ],
+                ),
+              ),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+    // ));
   }
 }
